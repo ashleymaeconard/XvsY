c1d47e01d89f07fd0c86a3087a612b5b04df4a2e
Ashley Conard <ashleymaeconard@gmail.com>
2020-10-08 11:22:21 -0400

    added comments for DESeq2 next steps

--- diffexpression.r
+++ diffexpression.r
@@ -1,12 +1,18 @@
 suppressMessages(library("DESeq2"))
 suppressMessages(library("pasilla"))
 cts <- as.matrix(read.csv("afterbatchtable_geneNameAdded.csv", row.names=1, stringsAsFactors=FALSE), stringsAsFactors=FALSE)
+
+#TODO: create cts object with non-corrected and normalized data 
+
 coldata <- read.csv("metadata.csv")
 coldata <- coldata[order(coldata$ID),]
 rownames(coldata) <- coldata$ID
 coldata$ID <- NULL
 head(cts,2)
 head(coldata,2)
+
+#TODO: PCA plot - label dots
+
 cts_filtered <- cts[,c("C4e.1","C4e.2","C4e.3","C4e.4","C6e.1","C6e.2","C6e.3","C6e.4")]
 cts_filtered <- transform(cts_filtered, C4e.1 = as.integer(C4e.1))
 cts_filtered <- transform(cts_filtered, C4e.2 = as.integer(C4e.2))
@@ -26,9 +32,9 @@ colnames(cts_filtered)
 nrow(coldata)
 rownames(coldata)
 sapply(cts_filtered, class)
-dds <- DESeqDataSetFromMatrix(countData = cts_filtered, colData = coldata, design = ~ condition)
+dds <- DESeqDataSetFromMatrix(countData = cts_filtered, colData = coldata, design = ~ condition) # + batch
 dds
-dds <- DESeq(dds, test="LRT", reduced = ~1)
+dds <- DESeq(dds, test="LRT", reduced = ~1) #batch
 res <- results(dds)
 res
 resultsNames(dds)
@@ -39,6 +45,8 @@ res <- res_no_padj[which(res_no_padj$padj < 0.01),]
 write.csv(res, file = 'deseq2_results_pvalfiltered1.csv')
 
 # Performing normal shrinkage transformation
+
+#TODO: AC figure out best shrinkage
 resNorm_no_padj <- lfcShrink(dds, coef=1, type="normal") # coef=1 is elav_gfp vs rnai 
 resNorm <- resNorm_no_padj[which(resNorm_no_padj$padj < 0.01),]
 write.csv(res, file = 'deseq2_results_pvalandnorm1.csv')
